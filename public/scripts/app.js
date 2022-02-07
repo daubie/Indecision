b3e666eea5c7143cf126c4d9c50706a50cd6b611
daubie <derek.k.aubie@gmail.com>
2022-02-07 09:14:55 -0500

    update babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,24 @@
 "use strict";
 
-var multiplier = {
-  numbers: [1, 2, 3, 4],
-  multiplyBy: 2,
-  multiply: function multiply() {
-    var _this = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this.multiplyBy;
-    });
-  }
+console.log('App.js is running!');
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer',
+  options: ['One', 'Two']
 };
-console.log(multiplier.multiply());
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, app.title), app.subtitle && /*#__PURE__*/React.createElement("p", null, app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are your options' : 'No options'), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item one"), /*#__PURE__*/React.createElement("li", null, "Item two")));
+var user = {
+  name: 'Andrew',
+  age: 26,
+  location: 'Philadelphia'
+};
+
+function getLocation(location) {
+  if (location) {
+    return /*#__PURE__*/React.createElement("p", null, "Location: ", location);
+  }
+}
+
+var templateTwo = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, user.name ? user.name : 'Anonymous'), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("p", null, "Age: ", user.age), getLocation(user.location));
+var appRoot = document.getElementById('app');
+ReactDOM.render(template, appRoot);
--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,10 +1,30 @@
-const multiplier = {
-  numbers: [1, 2, 3, 4],
-  multiplyBy: 2,
-  multiply() {
-    return this.numbers.map((number) =>
-      number * this.multiplyBy)
-  }
-}
+// arguments object - no longer bound with arrow functions
 
-console.log(multiplier.multiply())
+const add = (a, b) => {
+  // console.log(arguments);
+  return a + b;
+};
+console.log(add(55, 1, 1001));
+
+// this keyword - no longer bound
+
+const user = {
+  name: 'Andrew',
+  cities: ['Philadelphia', 'New York', 'Dublin'],
+  printPlacesLived() {
+    return this.cities.map((city) => this.name + ' has lived in ' + city);
+  }
+};
+console.log(user.printPlacesLived());
+
+// Challenge area
+
+const multiplier = {
+  numbers: [10, 20, 30],
+  multiplyBy: 3,
+  multiply() {
+    return this.numbers.map((number) => number * this.multiplyBy);
+  }
+};
+
+console.log(multiplier.multiply());
